d9e416318db349ab4240f6d0c60a48a2aae6030e
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-08-27 21:42:27 -0400

    refactor security_check

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ class BronzeBot:
                 cell = self.board[scan_pos]
                 halite[scan_pos] = cell.halite
 
-                if cell.ship or cell.shipyard:
+                if scan_pos != unit.position and (cell.ship or cell.shipyard):
                     if cell.ship:
                         if cell.ship.player == self.me:
                             ally_ship.append(cell.position)
@@ -116,17 +116,21 @@ class BronzeBot:
         move_x, move_y = unify_pos(des, self.size) - ship.position
 
         # Check direction availability & Add candidate action
-        next_move = [np.sign(move_x, 0), np.sign(0, move_y)]
+        next_move = [(np.sign(move_x), 0), (0, np.sign(move_y))]
         for move in next_move:
             next_pos = ship.position + move
-            if self.board[next_pos].ship or self.board[next_pos].shipyard:
-                break
-            if self.security_check(ship, dis=1, pos=next_pos):
+            next_cell = self.board[next_pos]
+
+            condition_1 = next_cell.ship and next_cell.ship.player != self.me and next_cell.ship.halite > ship.halite
+            condition_2 = next_cell.shipyard and next_cell.shipyard.player != self.me
+            condition_3 = self.security_check(ship, dis=1, pos=next_pos)
+
+            if (condition_1 or condition_2) and condition_3:
                 candidate_action.append(self.SHIP_ACTION_DICT[move])
 
         # Randomly choose an action
         if candidate_action:
-            ship.next_action = random.choice([candidate_action])
+            ship.next_action = random.choice(candidate_action)
 
     def course_reversal(self, ship: Ship):
         """
@@ -139,18 +143,25 @@ class BronzeBot:
         ]
         self.navigate(ship, Point(nearest_shipyard_x, nearest_shipyard_y))
 
-    def security_check(self, ship: Ship, dis: int = 1, pos: Point = None) -> bool:
+    def security_check(self, ship: Ship, dis: int = 1, pos: Point = None) -> list:
         """
         Check if ship is clear in given distance.
+        Args:
+            ship: Ship
+            dis: Int, Default = 1. The distance of security_check.
+            pos: Point, Default = None. If is given, then take pos as the security check center.
+
+        Returns: List of close enemy, if clear then return an empty list.
         """
         radar = self.unit_radar[ship.id]
+        close_enemy = []
         if not pos:
             pos = ship.position
         for enemy_pos in radar['enemy_ship']:
             enemy_ship = self.board[enemy_pos]
             if cal_dis(pos, enemy_pos) <= dis and ship.halite >= enemy_ship.halite:
-                return False
-        return True
+                close_enemy.append(enemy_pos)
+        return close_enemy
 
     def explore_command(self, ship: Ship, radar: dict):
         """
@@ -168,7 +179,7 @@ class BronzeBot:
         else:
             # If there's no halite, expand radar distance
             if max_free_halite == 0:
-                self.command(ship, radar['dis'] + 1)
+                self.ship_command(ship, radar['dis'] + 1)
             else:
                 candidate = []
                 for pos, free_halite in radar['free_halite'].items():
@@ -179,7 +190,7 @@ class BronzeBot:
                 des = random.choice(candidate)
                 self.navigate(ship, des)
 
-    def command(self, ship: Ship, radar_dis: int = 2, deposit_halite: int = 500, security_dis: int = 1):
+    def ship_command(self, ship: Ship, radar_dis: int = 2, deposit_halite: int = 500, security_dis: int = 1):
         """
         For each turn, update action of each ship.
 
@@ -193,6 +204,10 @@ class BronzeBot:
         self.radar(ship, radar_dis)
         radar = self.unit_radar[ship.id]
 
+        # Assign EXPLORE to new ship
+        if ship.id not in self.ship_state:
+            self.ship_state[ship.id] = 'EXPLORE'
+
         # DEPOSIT
         # Strategy 1: if ship is in a shipyard, and ship.halite is 0, turn DEPOSIT to EXPLORE
         # Strategy 2: if ship state is DEPOSIT, navigate to nearest shipyard
@@ -202,14 +217,14 @@ class BronzeBot:
             # If ship has deposited halite to shipyard, assign EXPLORE to ship.
             if ship.cell.shipyard and ship.halite == 0:
                 self.ship_state[ship.id] = 'EXPLORE'
-                self.command(ship, radar_dis, deposit_halite, security_dis)
+                self.ship_command(ship, radar_dis, deposit_halite, security_dis)
 
             if ship.halite >= deposit_halite:
                 self.course_reversal(ship)
             else:
-                if self.security_check(ship, security_dis):
+                if not self.security_check(ship, security_dis):
                     self.ship_state[ship.id] = 'EXPLORE'
-                    self.command(ship, radar_dis, deposit_halite, security_dis)
+                    self.ship_command(ship, radar_dis, deposit_halite, security_dis)
                 else:
                     # Enemy is close, stick to DEPOSIT ship state.
                     self.course_reversal(ship)
@@ -224,9 +239,9 @@ class BronzeBot:
         elif self.ship_state[ship.id] == 'COLLECT':
             if ship.halite >= deposit_halite:
                 self.ship_state[ship.id] = 'DEPOSIT'
-                self.command(ship, radar_dis, deposit_halite, security_dis)
+                self.ship_command(ship, radar_dis, deposit_halite, security_dis)
             else:
-                if self.security_check(ship, security_dis):
+                if not self.security_check(ship, security_dis):
                     self.explore_command(ship, radar)
                 else:
                     self.ship_state[ship.id] = 'DEPOSIT'
@@ -264,11 +279,20 @@ class BronzeBot:
         """
         Main Function
         """
+        print('MY TURN')
         self.update_map()
 
+        print('- update map')
+
         self.convert_command()
+        print('- convert command ')
         for ship in self.me.ships:
-            self.command(ship, radar_dis, deposit_halite, security_dis)
+            print('-- command {}'.format(ship.id))
+            self.ship_command(ship, radar_dis, deposit_halite, security_dis)
+            print('---- ship state: {}'.format(self.ship_state[ship.id]))
+            print('---- ship action: {}'.format(ship.next_action))
+
         self.spawn_command(max_ship)
+        print('- spawn command')
 
         return self.me.next_actions
