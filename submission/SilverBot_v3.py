53cee722d715dbcf65d23bafb6a283593391b5ba
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-09-11 22:16:45 -0400

    silverbot V3

--- submission/SilverBot_v3.py
+++ submission/SilverBot_v3.py
@@ -1,12 +1,70 @@
-import itertools
-import random
-from typing import Union
 
+import random
+
+from kaggle_environments.envs.halite.helpers import *
 import numpy as np
 
-from bot.base import Bot
-from helper import *
-from kaggle_helpers import *
+
+###################
+# Helper Function #
+###################
+
+
+def index_to_position(index: int, size: int):
+    """
+    Converts an index in the observation.halite list to a 2d position in the form (x, y).
+    """
+    y, x = divmod(index, size)
+    return Point(x, (size - y - 1))
+
+
+# TODO: refactor for vectorized calculation
+def cal_dis(x, y):
+    """
+    Calculate Manhattan Distance for two points
+    """
+    return sum(abs(x - y))
+
+
+def estimate_gain(halite, dis, t, collect_rate=0.25, regen_rate=0.02):
+    """
+    Calculate halite gain for given number of turn.
+    """
+    if dis >= t:
+        return 0
+    else:
+        # Halite will regenerate before ship arrives
+        new_halite = halite * (1 + regen_rate) ** max(0, dis - 1)
+        # Ship costs (dis) rounds to arrive destination, uses (t - dis) rounds to collect halite
+        return new_halite * (1 - (1 - collect_rate) ** (t - dis))
+
+
+def unify_pos(pos, size):
+    """
+    Convert position into standard one.
+    Example: Given size = 5, Point(-2, -7) -> Point(3, 3)
+    """
+    return pos % size
+
+
+def get_shorter_move(move, size):
+    """
+    Given one dimension move (x or y), return the shorter move comparing with opposite move.
+    The Board is actually round, ship can move to destination by any direction.
+    Example: Given board size = 5, move = 3, opposite_move = -2, return -2 since abs(-2) < abs(3).
+    """
+    if move == 0:
+        return 0
+    elif move > 0:
+        opposite_move = move - size
+    else:
+        opposite_move = move + size
+    return min([move, opposite_move], key=abs)
+    
+
+#############
+# Bot Class #
+#############
 
 
 class SilverBot:
@@ -369,9 +427,9 @@ class SilverBot:
             empty_shipyard.sort(key=lambda x: np.sum(
                 list(self.unit_radar[x.id]['free_halite'].values())
             ))
-
+    
             # Dynamically control the max_num_ship.
-            # Strategy_1:
+            # Strategy_1: 
             # Strategy_2:
             # Strategy_3:
             if 100 <= self.obs.step < 350:
@@ -396,7 +454,7 @@ class SilverBot:
             # If game is coming to an end, stop spawning new ships aggressively.
             elif self.obs.step >= 350:
                 max_num_ship *= 0.25
-
+    
             new_ship = 0
             # Spawn Condition:
             # 1. There are available empty shipyards.
@@ -469,3 +527,14 @@ class SilverBot:
                 # print('---- ship halite: {}'.format(ship.halite))
 
         return self.me.next_actions
+    
+    
+############
+# Launcher #
+############
+    
+
+def agent(obs,config):
+    bot = SilverBot(obs,config)
+    actions = bot.play(radar_dis=2, deposit_halite=500, security_dis=1, convert_sum=1500, max_ship=20)
+    return actions
